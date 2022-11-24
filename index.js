0aa7aeee08c3ddc42a672a3262ddd729537b79aa
Mark Boyd <markalanboyd@gmail.com>
2022-11-24 18:36:06 +0100

    Added winners tracker

--- index.js
+++ index.js
@@ -12,14 +12,30 @@ function reroll() {
     if (p1Roll > p2Roll) {
         document.querySelector("#p1 .card-header").style.backgroundColor = "lightgreen";
         document.querySelector("#p2 .card-header").style.backgroundColor = "tomato";
+        let icon = document.createElement("i");
+        let footer = document.querySelector("#p1 .card-footer");
+        icon.setAttribute("class", `bi bi-dice-${p1Roll} dice winners`);
+        footer.appendChild(icon);
     } else if (p1Roll === p2Roll) {
         document.querySelector("#p1 .card-header").style.backgroundColor = "gainsboro";
         document.querySelector("#p2 .card-header").style.backgroundColor = "gainsboro";
     } else {
         document.querySelector("#p1 .card-header").style.backgroundColor = "tomato";
         document.querySelector("#p2 .card-header").style.backgroundColor = "lightgreen";
+        let icon = document.createElement("i");
+        let footer = document.querySelector("#p2 .card-footer");
+        icon.setAttribute("class", `bi bi-dice-${p2Roll} dice winners`);
+        footer.appendChild(icon);
     }
 }
+
+// TODO: Combine the functionality above into a function somehow
+// function postWinner(winner, playerRoll) {
+//     let icon = document.createElement("i");
+//     let footer = document.querySelector(`${winner} .card-footer`);
+//     icon.setAttribute("class", `bi bi-dice-${playerRoll} dice`);
+//     footer.appendChild(icon);
+// }
 
 // Events
 buttonReroll.addEventListener("click", reroll)
